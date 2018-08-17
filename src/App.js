be21d4645594526c2db8f298cbd36440fecd435b
Igor Naumov <igor.naumov.17@gmail.com>
2018-08-17 09:59:58 +0300

    Setup containers folder and Home container

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
+import Home from './containers/home';
+
 import './styles/base.scss';
 
 const App = () => (
   <div>
-    <h1>App</h1>
+    <Home />
   </div>
 );
 
---
+++ src/containers/home/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import styles from './styles.scss';
+
+const Home = () => (
+  <div className={styles.container}>
+    <h1 className={styles.title}>Git API React</h1>
+  </div>
+);
+
+export default Home;
