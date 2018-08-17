e5f553695edaf5e28c78abf88262d52b3dd7a7ff
Igor Naumov <igor.naumov.17@gmail.com>
2018-08-17 19:01:19 +0300

    Added initial fetchRepos() call

--- src/containers/home/index.js
+++ src/containers/home/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
@@ -7,38 +7,46 @@ import { repoActions } from 'redux/actions';
 import { ReposList, Form } from './components';
 import styles from './styles.scss';
 
-/* eslint-disable no-console */
-const Home = ({ loading, error, items, fetchRepos, updateRepo }) => (
-  <div className={styles.container}>
-    <h1 className={styles.title}>Git API React</h1>
+class Home extends PureComponent {
+  static propTypes = {
+    loading: PropTypes.bool,
+    error: PropTypes.bool,
+    items: PropTypes.arrayOf(PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      name: PropTypes.string,
+      description: PropTypes.string,
+      url: PropTypes.string,
+      bookmarked: PropTypes.bool,
+    })).isRequired,
+    fetchRepos: PropTypes.func.isRequired,
+    updateRepo: PropTypes.func.isRequired,
+  }
 
-    <div className={styles.content}>
-      <Form
-        onChange={({ name, bookmarked }) => fetchRepos({ name, bookmarked })}
-      />
+  componentDidMount() {
+    const { fetchRepos } = this.props;
+    fetchRepos();
+  }
 
-      <ReposList
-        loading={loading}
-        error={error}
-        items={items}
-        onBookmarkToggle={(id, bookmarked) => updateRepo(id, { bookmarked })}
-      />
-    </div>
-  </div>
-);
+  render() {
+    const { loading, error, items, fetchRepos, updateRepo } = this.props;
 
-Home.propTypes = {
-  loading: PropTypes.bool,
-  error: PropTypes.bool,
-  items: PropTypes.arrayOf(PropTypes.shape({
-    id: PropTypes.number.isRequired,
-    name: PropTypes.string,
-    description: PropTypes.string,
-    url: PropTypes.string,
-    bookmarked: PropTypes.bool,
-  })).isRequired,
-  fetchRepos: PropTypes.func.isRequired,
-  updateRepo: PropTypes.func.isRequired,
+    return (
+       <div className={styles.container}>
+         <h1 className={styles.title}>Git API React</h1>
+         <div className={styles.content}>
+           <Form
+             onChange={({ name, bookmarked }) => fetchRepos({ name, bookmarked })}
+           />
+           <ReposList
+             loading={loading}
+             error={error}
+             items={items}
+             onBookmarkToggle={(id, bookmarked) => updateRepo(id, { bookmarked })}
+           />
+         </div>
+       </div>
+     );
+   }
 }
 
 const mapStateToProps = (state) => ({
